22f0e5ef369aa2c1a744bb74acf33762b822755d
cmwedin <cmwedin@gmail.com>
2022-09-25 16:10:21 -0400

    squarestuct blittalble fix
    changes bools for corners to ints to be blittable, will be 0 for false and 1 for true

--- Assets/Scripts/MarchingSquares/SquareStruct.cs
+++ Assets/Scripts/MarchingSquares/SquareStruct.cs
@@ -6,8 +6,8 @@ public struct SquareStruct
 {
     //? 4dig binary number representing with 0 for corners outside and 1 for corners inside (starting from bottom left proceeding clockwise)
     public int squareType;
-    public bool bottomLeft;
-    public bool topLeft;
-    public bool topRight;
-    public bool bottomRight;
+    public int bottomLeft;
+    public int topLeft;
+    public int topRight;
+    public int bottomRight;
 }
